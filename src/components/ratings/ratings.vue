ac26cd1b9bb02edc10fc3f21e875666a4e0dfeb5
itcast <itheima@itcast.cn>
2020-06-02 00:14:01 +0800

    third commit

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ratings">
+  <div class="ratings" ref='ratings'>
     <div class="rating-content">
       <div class='overview'>
         <div class="overview-left">
@@ -25,23 +25,26 @@
         </div>
       </div>
       <split></split>
-      <ratingselect :select-type='selectType' :only-content='onlyContent' :desc='desc' :ratings='ratings'></ratingselect>
+      <ratingselect @updateselectType='updateselectType'  @updateonlyContent='updateonlyContent' :select-type='selectType' :only-content='onlyContent' :desc='desc' :ratings='ratings'></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li v-for='(rating, index) in ratings' :key='index'>
+          <li v-show='needShow(rating.rateType, rating.text)' v-for='(rating, index) in ratings' :key='index' class='rating-item border-1px'>
             <div class="avatar">
-              <img :src=rating.avatar alt="">
+              <img width="28" height="28" :src=rating.avatar alt="">
             </div>
             <div class="content">
               <h1 class="name">{{rating.username}}</h1>
-              <div class="star wrapper">
-                <star :size='24' :score='rating.score'></star>
+              <div class="star-wrapper">
+                <star class='star' :size='24' :score='rating.score'></star>
                 <span class='delivery' v-show='rating.deliveryTime'>{{rating.deliveryTime}}</span>
               </div>
               <p class='text'>{{rating.text}}</p>
               <div class="recommend" v-show='rating.recommend && rating.recommend.length'>
                 <span class='icon-thumb_up'></span>
-                <span v-for='(recommend, index) in rating.recommend' :key='index'>{{recommend}}</span>
+                <span class='item' v-for='(recommend, index) in rating.recommend' :key='index'>{{recommend}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
               </div>
             </div>
           </li>
@@ -55,6 +58,8 @@
   import star from 'components/star/star';
   import split from 'components/split/split';
   import ratingselect from 'components/ratingselect/ratingselect';
+  import BScroll from 'better-scroll';
+  import {formatDate} from '../../common/js/date';
 
   const ALL = 2;
   const ERR_OK = 0;
@@ -84,16 +89,55 @@
           this.ratings = res.data;
         }
       });
+      // 页面更新完成后执行
+      this.$nextTick(() => {
+          // 给ratings添加滚动效果
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.ratings, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
     },
     components: {
       star,
       split,
       ratingselect
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+    },
+    methods: {
+      // 不同类型评论展示效果
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      },
+       // 更新子组件传递的selectType
+       updateselectType(type) {
+        this.selectType = type;
+      },
+      updateonlyContent(onlyContent) {
+        this.onlyContent = onlyContent;
+      }
     }
   };
 </script>
 
 <style lang="stylus" scoped>
+  @import '../../common/stylus/mixin.styl';
+
   .ratings
     position: absolute
     top: 174px
@@ -161,5 +205,64 @@
           .delivery
             margin-left: 12px
             font-size: 12px
+            color: rgb(147, 153, 159)
+    .rating-wrapper
+      padding: 0 18px
+      .rating-item
+        display: flex
+        padding: 18px 0
+        border-1px(rgba(7,17,27,0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-right: 12px
+          img
+            border-radius: 50%
+        .content
+          position: relative
+          flex: 1
+          .name
+            margin-bottom: 4px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(7,17,27)
+          .star-wrapper
+            margin-bottom: 6px
+            font-size: 0
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              display: inline-block
+              vertical-algin: top
+              line-height: 12px
+              font-size: 10px
+              color: rgb(7,17,27)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color: rgb(7,17,27)
+            font-size: 12px
+          .recommend
+            line-height: 16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0,160,220)
+            .item
+              margin: 6px
+              border: 1px solid rgba(7,17,27,0.1)
+              border-radius: 1px
+              color: rgb(147, 153, 159)
+              background-color: #fff
+          .time
+            position: absolute
+            top: 0
+            right: 0
+            font-size: 10px
             color: rgb(147, 153, 159)
 </style>
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,13 +1,83 @@
 <template>
   <div class="seller">
-    我是商家
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class='title'>{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size='36' :score='seller.score'></star>
+          <span class='text'>({{seller.ratingCount}})</span>
+          <span class='text'>{{seller.sellCount}}</span>
+        </div>
+        <ul class="remark">
+          <li class='block'>
+            <h2>起送价</h2>
+            <div class="content">
+              <span class='stress'>{{seller.minPrice}}元</span>
+            </div>
+          </li>
+          <li class='block'>
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class='stress'>{{seller.deliveryPrice}}元</span>
+            </div>
+          </li>
+          <li class='block'>
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class='stress'>{{seller.deliveryTime}}分钟</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+  import star from 'components/star/star';
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    components: {
+      star
+    }
+  };
 </script>
 
-<style lang="scss" scoped>
-.seller{}
+<style lang="stylus" scoped>
+  @import '../../common/stylus/mixin.styl';
+
+  .seller
+    position: absolute
+    top: 174px
+    left: 0
+    bottom: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7,12,27)
+      .desc
+        padding: 18px
+        line-height: 18px
+        font-size: 0
+        border-1px(rgba(7,17,27,0.1))
+        .star
+          display: inline-block
+          margin-right: 8px
+          vartical-align: top
+        .text
+          margin-right: 12px
+          line-height: 18px
+          font-size: 10px
+          vartical-align: top
+          color: rgb(77,85,93)
 </style>
